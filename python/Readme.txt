640762b1a808c264d72146383bff6241645d78c4
Xiaofeng Zan <xfzan@thoughtworks.com>
2019-11-16 10:38:34 +0800

    fix bug

--- python/Readme.txt
+++ python/Readme.txt
@@ -6,6 +6,8 @@ sys.path
 
 #调试器模式启动
 python3 -m pdb <script>.py
+#启动后程序暂停在第一行，按c继续，按Ctol+c暂停
+#暂停后输入where或者bt列出栈信息
 
 #Profile模式启动
 python3 -m profile <script>.py
---
+++ python/forDebuger.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-  
+
+import time
+
+def func_a():
+    print("in function a")
+
+def func_b():
+    print("in function b")
+
+def func_c():
+    print("in function c")
+
+def func_d():
+    print("in function d")
+    counter = 0
+    while True:
+        func_e(counter)
+        counter += 1
+
+def func_e(counter):
+    print("in function e " + str(counter))
+    time.sleep(.2)
+
+def main():
+    func_a()
+    func_b()
+    func_c()
+    func_d()
+
+main()

87248d02f8f46d8c511eae08c12a7ab90d8eb846
davis <zanxiaofeng@163.com>
2022-01-14 17:15:29 +0800

    update unicode

--- java/UnicodeCases.java
+++ java/UnicodeCases.java
@@ -2,6 +2,29 @@ package zxf;
 
 import static java.nio.charset.StandardCharsets.*;
 
+/*
+ UTF-8 has the property that all existing 7-bit ASCII strings are still valid. UTF-8 only affects the meaning of bytes greater than 127, which it uses to represent higher Unicode characters. A character might require 1, 2, 3, or 4 bytes of storage depending on its value; more bytes are needed as values get larger. To store the full range of possible 32-bit characters, UTF-8 would require a whopping 6 bytes. But again, Unicode only defines characters up to 0x10FFFF, so this should never happen in practice.
+
+UTF-8 is a specific scheme for mapping a sequence of 1-4 bytes to a number from 0x000000 to 0x10FFFF:
+
+00000000 -- 0000007F:   0xxxxxxx
+00000080 -- 000007FF:   110xxxxx 10xxxxxx
+00000800 -- 0000FFFF:   1110xxxx 10xxxxxx 10xxxxxx
+00010000 -- 001FFFFF:   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+The x's are bits to be extracted from the sequence and glued together to form the final number.
+
+It is fair to say that UTF-8 is taking over the world. It is already used for filenames in Linux and is supported by all mainstream web browsers. This is not surprising considering its many nice properties:
+
+    It can represent all 1,114,112 Unicode characters.
+    Most C code that deals with strings on a byte-by-byte basis still works, since UTF-8 is fully compatible with 7-bit ASCII.
+    Characters usually require fewer than four bytes.
+    String sort order is preserved. In other words, sorting UTF-8 strings per-byte yields the same order as sorting them per-character by logical Unicode value.
+    A missing or corrupt byte in transmission can only affect a single character—you can always find the start of the sequence for the next character just by scanning a couple bytes.
+    There are no byte-order/endianness issues, since UTF-8 data is a byte stream.
+
+*/
+
 public class UnicodeCases {
     public static void main(String[] args) {
         String chineseInJava = "ceshi";
